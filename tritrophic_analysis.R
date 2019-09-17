183ffe3b0793378477799a048b481e21e504739d
Christie Bahlai <cbahlai@kent.edu>
2019-09-17 13:55:36 -0400

    create a folder for secondary data products, start into the grasshoppers

--- tritrophic_analysis.R
+++ tritrophic_analysis.R
@@ -69,4 +69,48 @@ grassmass.irrigated.forbs$avg.LIVEGRASS<-NULL
 grassmass.irrigated.forbs$PLOT<-NULL
 grassmass.irrigated.forbs$TRANSECT<-NULL
 
-pyramid_plot(grassmass.irrigated.forbs)
+#now let's write the intermediate cleaned data products into a folder
+
+#we want to encode the information about site and trophic level into the file name
+write.csv(grassmass.control.grass, file="cleaned_data/Konza_producer_control_grass.csv")
+write.csv(grassmass.control.forbs, file="cleaned_data/Konza_producer_control_forbs.csv")
+write.csv(grassmass.irrigated.grass, file="cleaned_data/Konza_producer_irrigated_grass.csv")
+write.csv(grassmass.irrigated.forbs, file="cleaned_data/Konza_producer_irrigated_forbs.csv")
+
+#ok, let's grasshopper this! the data has a BUNCH of issues we're going to have to address, 
+#but let's talk about that later and bring it in first
+
+hoppers<-read.csv(file="https://portal.lternet.edu/nis/dataviewer?packageid=knb-lter-knz.29.12&entityid=3fb352e2478f776517f7e880fe31b808", 
+                    header=T, na.strings=c("",".","NA"))
+
+summary(hoppers)
+
+#oooh boy. well, our first problem is that the datasheet doesn't record zeros, just blanks
+#but we can kind of get around it by assuming that the total column is correct.
+#because of the way the data is recorded, we're going to have to make the big, horrible
+#assumption that in the unit of 200 sweeps, we always got at least one grasshopper, because the 
+#way this is actually set up, if a researcher went out and swept and got nothing, they actually would
+#record nothing. I think we should do the top four or so species (let's see what it looks like when
+#we actually get counts by species) so we can use evidence that other species were there in a sample
+#to essentially generate the absences of the key species we select
+
+#first, let's cull out data from before 1996, because we need a continuous series anyway, so this cuts out the 
+#hole in the data
+
+hoppers1<-hoppers[which(hoppers$RECYEAR>1995),]
+
+#let's also cull out the breakdown by units of sweeps
+hoppers1$S1<-NULL
+hoppers1$S2<-NULL
+hoppers1$S3<-NULL
+hoppers1$S4<-NULL
+hoppers1$S5<-NULL
+hoppers1$S6<-NULL
+hoppers1$S7<-NULL
+hoppers1$S8<-NULL
+hoppers1$S9<-NULL
+hoppers1$S10<-NULL
+#and spcode and soiltype
+hoppers1$SPCODE<-NULL
+hoppers1$SOILTYPE<-NULL
+
