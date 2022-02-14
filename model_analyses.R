9798d805f0f494d61cd5f3cf9318c243fdaa1650
Kayla I Perry <kiperry1488@gmail.com>
2022-02-14 14:40:51 -0500

    added additional graphs

--- model_analyses.R
+++ model_analyses.R
@@ -36,6 +36,7 @@ mod.lter$trophic_level <- factor(mod.lter$trophic_level,
                                  levels = c("producer", "herbivore", "consumer", "omnivore", "predator"))
 levels(mod.lter$trophic_level)
 mod.lter$N_years <- as.numeric(mod.lter$N_years)
+mod.lter$start_year <- as.numeric(mod.lter$start_year)
 
 png("figures/LTER_SITES.png", width = 2000, height = 600, pointsize = 20)
 xyplot(mod.lter$N_years ~ mod.lter$slope | mod.lter$trophic_level,
@@ -45,6 +46,12 @@ xyplot(mod.lter$N_years ~ mod.lter$slope | mod.lter$trophic_level,
        xlab = "Slope", ylab = "Number of years",
        abline=c(v=0, lwd = 3))
 dev.off()
+
+xyplot(mod.lter$slope ~mod.lter$start_year | mod.lter$trophic_level,
+       col = ifelse(mod.lter$p_value < 0.05,'red','black'),
+       pch = 19, cex = 1,
+       strip = strip.custom(bg="lightgrey", par.strip.text=list(col="black", cex = 1, font = 2)),
+       xlab = "Start Year", ylab = "Slope")
 
 
 ## Konza Prairie
@@ -60,6 +67,7 @@ levels(mod.konza$trophic_level)
 mod.konza$trophic_level <- factor(mod.konza$trophic_level, levels = c("producer", "herbivore", "omnivore"))
 levels(mod.konza$trophic_level)
 mod.konza$N_years <- as.numeric(mod.konza$N_years)
+mod.konza$start_year <- as.numeric(mod.konza$start_year)
 
 png("figures/Konza_Prairie.png", width = 1600, height = 600, pointsize = 20)
 xyplot(mod.konza$N_years ~ mod.konza$slope | mod.konza$trophic_level,
@@ -69,6 +77,12 @@ xyplot(mod.konza$N_years ~ mod.konza$slope | mod.konza$trophic_level,
        xlab = "Slope", ylab = "Number of years",
        abline=c(v=0, lwd = 3))
 dev.off()
+
+xyplot(mod.konza$slope ~ mod.konza$start_year | mod.konza$trophic_level,
+       col = ifelse(mod.konza$p_value < 0.05,'red','black'),
+       pch = 19, cex = 1,
+       strip = strip.custom(bg="lightgrey", par.strip.text=list(col="black", cex = 1, font = 2)),
+       xlab = "Start Year", ylab = "Slope", abline=c(h=0, lwd = 3))
 
 ggplot(mod.konza, aes(N_years, slope_se, colour = trophic_level)) + geom_point() + theme_classic()
 ggplot(mod.konza, aes(N_years, p_value, colour = trophic_level)) + geom_point() + theme_bw()
@@ -88,6 +102,7 @@ levels(mod.hbrook$trophic_level)
 mod.hbrook$trophic_level <- factor(mod.hbrook$trophic_level, levels = c("producer", "herbivore", "omnivore"))
 levels(mod.hbrook$trophic_level)
 mod.hbrook$N_years <- as.numeric(mod.hbrook$N_years)
+mod.hbrook$start_year <- as.numeric(mod.hbrook$start_year)
 
 png("figures/Hubbard_Brook.png", width = 1600, height = 600, pointsize = 20)
 xyplot(mod.hbrook$N_years ~ mod.hbrook$slope | mod.hbrook$trophic_level,
@@ -97,6 +112,12 @@ xyplot(mod.hbrook$N_years ~ mod.hbrook$slope | mod.hbrook$trophic_level,
        xlab = "Slope", ylab = "Number of years",
        abline=c(v=0, lwd = 3))
 dev.off()
+
+xyplot(mod.hbrook$slope ~ mod.hbrook$start_year | mod.hbrook$trophic_level,
+       col = ifelse(mod.hbrook$p_value < 0.05,'red','black'),
+       pch = 19, cex = 1,
+       strip = strip.custom(bg="lightgrey", par.strip.text=list(col="black", cex = 1, font = 2)),
+       xlab = "Start Year", ylab = "Slope", abline=c(h=0, lwd = 3))
 
 
 ## North Temperate Lakes
@@ -111,6 +132,7 @@ levels(mod.ntlakes$trophic_level)
 mod.ntlakes$trophic_level <- factor(mod.ntlakes$trophic_level, levels = c("producer", "consumer", "predator"))
 levels(mod.ntlakes$trophic_level)
 mod.ntlakes$N_years <- as.numeric(mod.ntlakes$N_years)
+mod.ntlakes$start_year <- as.numeric(mod.ntlakes$start_year)
 
 png("figures/North_Temperate_Lakes.png", width = 1600, height = 600, pointsize = 20)
 xyplot(mod.ntlakes$N_years ~ mod.ntlakes$slope | mod.ntlakes$trophic_level,
@@ -120,6 +142,12 @@ xyplot(mod.ntlakes$N_years ~ mod.ntlakes$slope | mod.ntlakes$trophic_level,
        xlab = "Slope", ylab = "Number of years",
        abline=c(v=0, lwd = 3))
 dev.off()
+
+xyplot(mod.ntlakes$slope ~ mod.ntlakes$start_year | mod.ntlakes$trophic_level,
+       col = ifelse(mod.ntlakes$p_value < 0.05,'red','black'),
+       pch = 19, cex = 1,
+       strip = strip.custom(bg="lightgrey", par.strip.text=list(col="black", cex = 1, font = 2)),
+       xlab = "Start Year", ylab = "Slope", abline=c(h=0, lwd = 3))
 
 
 ## North Temperate Lakes
@@ -134,6 +162,7 @@ levels(mod.sbcoastal$trophic_level)
 mod.sbcoastal$trophic_level <- factor(mod.sbcoastal$trophic_level, levels = c("producer", "consumer", "predator"))
 levels(mod.sbcoastal$trophic_level)
 mod.sbcoastal$N_years <- as.numeric(mod.sbcoastal$N_years)
+mod.sbcoastal$start_year <- as.numeric(mod.sbcoastal$start_year)
 
 png("figures/Santa_Barbara_Coastal.png", width = 1600, height = 600, pointsize = 20)
 xyplot(mod.sbcoastal$N_years ~ mod.sbcoastal$slope | mod.sbcoastal$trophic_level,
@@ -144,4 +173,9 @@ xyplot(mod.sbcoastal$N_years ~ mod.sbcoastal$slope | mod.sbcoastal$trophic_level
        abline=c(v=0, lwd = 3))
 dev.off()
 
+xyplot(mod.sbcoastal$slope ~ mod.sbcoastal$start_year | mod.sbcoastal$trophic_level,
+       col = ifelse(mod.sbcoastal$p_value < 0.05,'red','black'),
+       pch = 19, cex = 1,
+       strip = strip.custom(bg="lightgrey", par.strip.text=list(col="black", cex = 1, font = 2)),
+       xlab = "Start Year", ylab = "Slope", abline=c(h=0, lwd = 3))
 
